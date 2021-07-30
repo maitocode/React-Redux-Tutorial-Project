99ed7034c864b79876d11d2c1ccab9958bc7d5cd
bluerain4 <code.maito@gmail.com>
2021-07-31 00:50:21 +0700

    will be done

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -1,8 +1,9 @@
 import { combineReducers } from "redux";
-import { productReducer } from "./productReducer.js";
+import { productReducer, selectedProductReducer } from "./productReducer.js";
 
 const reducers = combineReducers({
   allProducts: productReducer,
+  product: selectedProductReducer,
 });
 
 export default reducers;
